d31b72fadd9047639364ae970196490b42e4996e
Md Azaz <mdazaz9026@gmail.com>
2023-11-06 22:44:19 +0530

    userSchema

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const mongoose  = require('mongoose')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-module.exports = router;
+mongoose.connect('mongodb://127.0.0.1:27017/Endgame2')
+
+
+const userSchema = mongoose.Schema({
+  username : String,
+  username : String,
+  description : String,
+  categories : {
+    type : Array,
+    default : []
+  },
+  datecreated : {
+    type : Date,
+    default : Date.now()
+  }
+
+})
+
