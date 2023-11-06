8cd71c83b96d93d71932e1bb26f54c53f0168ab6
Md Azaz <mdazaz9026@gmail.com>
2023-11-06 22:30:00 +0530

    GET Contact page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET Home page. */
+
+router.get('/', function(req, res) {
+  a = 12;
+  res.render('index');
+});
+
+/* GET Contact page. */
+
+router.get('/failed', function(req, res) {
+  req.flash("age" , 12)
+  req.flash("name" , "Md Azaz")
+  res.send('bangaya')
+});
+
+router.get('/checkkaro', function(req, res) {
+  console.log(req.flash('age'), req.flash('name'))
+  res.send('check karlo be ke terminal par')
 });
 
 module.exports = router;
